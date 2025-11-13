2dd479f198bdf09d9eb65c7438fe1c9b6a354f7e
Hussein-Yaacoub <hay23@mail.aub.edu>
2025-11-13 14:11:02 +0200

    Initial Jenkins project files

---
+++ Jenkins_project/app.py
@@ -0,0 +1,7 @@
+# app.py
+def greet(name):
+    return f"Helloz, {name}!"
+
+
+if __name__ == "__main__":
+    print(greet("World"))
---
+++ Jenkins_project/tests/test_app.py
@@ -0,0 +1,12 @@
+# tests/test_app.py
+import unittest
+from Jenkins_project.app import greet
+
+
+class TestApp(unittest.TestCase):
+    def test_greet(self):
+        self.assertEqual(greet("Hussein Yaacoub"), "Helloz, Hussein Yaacoub!")
+
+
+if __name__ == "__main__":
+    unittest.main()
