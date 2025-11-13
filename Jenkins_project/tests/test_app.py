b4f243b18ec894f67080bc98a00db14f02f76a89
Hussein-Yaacoub <hay23@mail.aub.edu>
2025-11-13 19:13:02 +0200

    Update test_app.py

--- Jenkins_project/tests/test_app.py
+++ Jenkins_project/tests/test_app.py
@@ -5,7 +5,7 @@ from Jenkins_project.app import greet
 
 class TestApp(unittest.TestCase):
     def test_greet(self):
-        self.assertEqual(greet("Hussein Yaacoub"), "Helloz, Hussein Yaacoub!")
+        self.assertEqual(greet("Hussein Yaacoub"), "Hello, Hussein Yaacoub!")
 
 
 if __name__ == "__main__":
